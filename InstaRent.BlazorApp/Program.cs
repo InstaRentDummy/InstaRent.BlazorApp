0ea510c9ccc77e2e9b66e354ff6d1e087173b5f0
winkaykay <winkaykaykhine@gmail.com>
2022-09-22 22:15:46 +0800

    Add project files.

---
+++ InstaRent.BlazorApp/Program.cs
@@ -0,0 +1,18 @@
+using InstaRent.BlazorApp;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:44316") });
+
+builder.Services.AddOidcAuthentication(options =>
+{
+    // Configure your authentication provider options here.
+    // For more information, see https://aka.ms/blazor-standalone-auth
+    builder.Configuration.Bind("Local", options.ProviderOptions);
+});
+
+await builder.Build().RunAsync();
---
+++ InstaRent.BlazorApp/Shared/Dto/PagedResultDto.cs
@@ -0,0 +1,8 @@
+﻿namespace InstaRent.BlazorApp.Shared.Dto
+{
+    public class PagedResultDto<T>
+    {
+        public long TotalCount { get; set; }
+        public IReadOnlyList<T> items { get; set; }
+    }
+}
