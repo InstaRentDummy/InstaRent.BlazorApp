95b32d3902ca04a51f3ff0dc1694fa96bb783312
arkar@assimilated.com.sg <arkar@assimilated.com.sg>
2022-09-28 15:56:47 +0800

    Update User Login

--- InstaRent.BlazorApp/Services/Bags/IBagService.cs
+++ InstaRent.BlazorApp/Services/Bags/IBagService.cs
@@ -1,16 +1,14 @@
 ﻿using InstaRent.BlazorApp.Shared.Bags;
-using InstaRent.Catalog.Bags;
 
 namespace InstaRent.BlazorApp.Services.Bags
 {
     public interface IBagService
     {
-        event Action OnChange;
         BagListDto Bags { get; set; }
         Task GetListByUserId(int currentPage, string userId, string name, string description, string tags, string status);
-        Task<BagDto> GetInfoById(string bagId);
-        Task<HttpResponseMessage> Create(BagDto BagDto);
-        Task<HttpResponseMessage> Update(BagDto BagDto);
+        Task<BagInfoDto> GetInfoById(string bagId);
+        Task<HttpResponseMessage> Create(BagInfoDto BagDto);
+        Task<HttpResponseMessage> Update(BagInfoDto BagDto);
         Task<HttpResponseMessage> Delete(string bagId);
     }
 }
--- InstaRent.BlazorApp/Services/Users/UserService.cs
+++ InstaRent.BlazorApp/Services/Users/UserService.cs
@@ -1,4 +1,5 @@
-﻿using InstaRent.Login.Users;
+﻿using InstaRent.BlazorApp.Shared.Users;
+using InstaRent.Login.Users;
 using System.Net.Http.Json;
 
 namespace InstaRent.BlazorApp.Services.Users
@@ -13,7 +14,7 @@ namespace InstaRent.BlazorApp.Services.Users
             _http = http;
         }
 
-        public async Task<UserDto?> Login(LoginUserDto user)
+        public async Task<UserInfoDto?> Login(UserLoginInfoDto user)
         {
             var dto = new LoginUserDto()
             {
@@ -25,19 +26,21 @@ namespace InstaRent.BlazorApp.Services.Users
 
             if (response.IsSuccessStatusCode)
             {
-                return response.Content.ReadFromJsonAsync<UserDto>().Result;
+                var result = response.Content.ReadFromJsonAsync<UserDto>().Result;
+
+                return ConvertInfo(result);
             }
             else
                 return null;
         }
 
-        public async Task<UserDto> GetInfoById(string userId)
+        public async Task<UserInfoDto> GetInfoById(string userId)
         {
-            var _userDto = await _http.GetFromJsonAsync<UserDto>($"{_url}/{userId}");
-            return _userDto == null ? new() : _userDto;
+            var _user = await _http.GetFromJsonAsync<UserDto>($"{_url}/{userId}");
+            return _user == null ? new() : ConvertInfo(_user);
         }
 
-        public async Task<HttpResponseMessage> Create(UserDto user)
+        public async Task<HttpResponseMessage> Create(UserInfoDto user)
         {
             var dto = new CreateUpdateUserDto()
             {
@@ -46,11 +49,11 @@ namespace InstaRent.BlazorApp.Services.Users
                 Password = user.Password,
                 Role = user.Role
             };
-            return await _http.PostAsJsonAsync<CreateUpdateUserDto>(_url, dto, CancellationToken.None);
+            return await _http.PostAsJsonAsync<CreateUpdateUserDto>($"{_url}/Register", dto, CancellationToken.None);
             //NavigationManager.NavigateTo("/product/Index");
         }
 
-        public async Task<HttpResponseMessage> Update(UserDto user)
+        public async Task<HttpResponseMessage> Update(UserInfoDto user)
         {
             return await _http.PutAsJsonAsync<CreateUpdateUserDto>($"{_url}/{user.Id}", new CreateUpdateUserDto()
             {
@@ -61,5 +64,19 @@ namespace InstaRent.BlazorApp.Services.Users
             });
         }
 
+        private UserInfoDto ConvertInfo(UserDto? dto)
+        {
+            UserInfoDto info = new UserInfoDto();
+
+            if (dto != null)
+            {
+                info.Id = dto.Id.ToString();
+                info.Name = dto.Name;
+                info.Email = dto.Email;
+                info.Role = dto.Role;
+            }
+
+            return info;
+        }
     }
 }
